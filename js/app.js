a272e01655dbb31af071cc3bb0047dba75ccc264
Anton Taraev <anti191@gmail.com>
2013-05-30 01:54:52 +0400

    Add generate form1

--- js/app.js
+++ js/app.js
@@ -1,38 +1,26 @@
 $(function() {
     var User = Backbone.Model.extend({
         schema: {
-            title:      { type: 'Select', options: ['', 'Mr', 'Mrs', 'Ms'] },
-            name:       { type: 'Text', validators: ['required'] },
-            email:      { validators: ['required', 'email'] },
-            birthday:   'Date',
-            password:   'Password',
-            notes:      { type: 'List', listType: 'Text' },
-
+            maf_last_name:       { type: 'Text', validators: ['required'] },
+            maf_email:       { type: 'Text', validators: ['email', 'required'] },
         }
     });
 
     var user = new User({
-        title: 'Mr',
-        name: 'Sterling Archer',
-        email: 'sterlingi@sis.com',
-        birthday: new Date(1978, 6, 12),
-        password: 'dangerzone',
-        notes: [
-            'Buy new turtleneck',
-            'Call Woodhouse',
-            'Buy booze'
-        ]
+        maf_last_name: 'Sterling',
+        maf_email: 'sterlingi@sis.com',
     });
 
     var form = new Backbone.Form({
+        template: _.template($('#form_block_1').html()),
         model: user
     }).render();
 
     form.on('change', function(form, titleEditor) {
-            console.log('Title changed to');
+            console.log('Title changed to'+form);
             form.validate();
     });
 
-    $('body').append(form.el);
+    $('.block_bg').append(form.el);
 });
 
